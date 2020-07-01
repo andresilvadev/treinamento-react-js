b23f86ef964b661afa124d3ac2c01df86535ca3e
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-07-01 00:55:46 -0300

    Criando botoes de Avançar e Próximo

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,8 @@ export default class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     /**
@@ -17,14 +19,36 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products') ;
-        console.log(response);
-        this.setState({ products: response.data.docs })
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`) ;
+        
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     };
 
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages ) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+
     render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
     return (
         <div className="product-list">
             { products.map(product => (
@@ -34,7 +58,11 @@ export default class Main extends Component {
                     <a href="">Acessar</a>
                 </article>                
             ))}
-        </div>
+            <div className="actions">
+                <button disabled={page ===1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={page === productInfo.page} onClick={this.nextPage}>Próximo</button>
+            </div>
+        </div>        
         );
     }
 }
